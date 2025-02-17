d7fd36de60125f09a7994bb15f15545c514fdc52
hssw24 <hssw@gmx.de>
2025-02-17 15:59:00 +0100

    Beschreibung der Änderungen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h3 style={{ textAlign: 'center', margin: '20px 0' }}>
-        Grundschule Afferde für ♥2a♥ von hssw
+        GS Afferde für  ♥2a♥  von hssw
 
       </h3>
       <MathLearningApp />
